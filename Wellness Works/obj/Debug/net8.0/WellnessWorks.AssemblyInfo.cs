343fcddfef52f7e8d2b04d390e5fe8edda793d28
Alex <alexandre.d.paquette+github@gmail.com>
2024-04-23 17:54:05 +0100

    Finished nav bar

--- Wellness Works/obj/Debug/net8.0/WellnessWorks.AssemblyInfo.cs
+++ Wellness Works/obj/Debug/net8.0/WellnessWorks.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WellnessWorks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f16cef3fd43aa634a534722a313d91bf2ba7a0ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a6273addb7c8b5578c0cf337fbd7bb58234180f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WellnessWorks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WellnessWorks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
