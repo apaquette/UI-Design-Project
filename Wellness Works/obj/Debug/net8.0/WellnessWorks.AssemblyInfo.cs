5b6b88b96ebd4d51359a672dd27b836511df1f33
Alex <alexandre.d.paquette+github@gmail.com>
2024-04-27 13:12:16 +0100

    made minor changes to UI, added settings page, added survey link

--- Wellness Works/obj/Debug/net8.0/WellnessWorks.AssemblyInfo.cs
+++ Wellness Works/obj/Debug/net8.0/WellnessWorks.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WellnessWorks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ee3b5ec016929d9036b8b5e6ebf239b0dbd9642")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5f57cfa6d410c48b7e7e109e3184e6d131170de")]
 [assembly: System.Reflection.AssemblyProductAttribute("WellnessWorks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WellnessWorks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
